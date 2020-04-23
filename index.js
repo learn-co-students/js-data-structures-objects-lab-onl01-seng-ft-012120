416dac4f7d2e37b762abf322fa954c699072da24
Ivana <ivana_pile@yahoo.com>
2020-04-23 18:43:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const driver = {}
+
+// returns a driver with the orignal key value pairs and the new
+// returns a clone with the new data - doesn't modify the original driver
+function updateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign({}, driver,  { [key]: value })
+}
+
+// updates `driver` with the given `key` and `value` (it is destructive, this means that we're making changes directly to the original Object
+// and returns the entire updated driver
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
+}
+// non-destructive
+function deleteFromDriverByKey(driver, key) {
+    const newObj = Object.assign({}, driver);
+    delete newObj[key];
+    return newObj;
+}
+
+// destructive
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
