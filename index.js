953f0957acd0f1295899590f1a770dc592a35ec8
github username <github email address>
2020-05-03 15:50:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+    let driver = {};
+    function updateDriverWithKeyAndValue(driver,key,value){
+    const newDriver = driver;  //newDriver{{}, key: value}
+    newDriver[key] = value;
+    return driver,newDriver;
+   
+}
